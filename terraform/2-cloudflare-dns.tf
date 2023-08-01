8c5216cabf8cd5d14f946061a18769e34858ac30
David Lee <leetdavidu@gmail.com>
2023-08-02 00:39:55 +0800

    added more CNAMEs

--- terraform/2-cloudflare-dns.tf
+++ terraform/2-cloudflare-dns.tf
@@ -1,10 +1,71 @@
+# https://registry.terraform.io/providers/cloudflare/cloudflare/latest/docs/resources/record
+resource "cloudflare_record" "unraid" {
+  zone_id = var.cloudflare_zone_id
+  name    = "unraid"
+  value   = "${var.cloudflare_tunnel_id}.cfargotunnel.com"
+  type    = "CNAME"
+  ttl     = 1
+  proxied = true
+
+  allow_overwrite = true
+}
+
+# https://registry.terraform.io/providers/cloudflare/cloudflare/latest/docs/resources/record
+resource "cloudflare_record" "traefik" {
+  zone_id = var.cloudflare_zone_id
+  name    = "traefik"
+  value   = "unraid.${local.domain_name}"
+  type    = "CNAME"
+  ttl     = 1
+  proxied = true
+
+  allow_overwrite = true
+}
 
 # https://registry.terraform.io/providers/cloudflare/cloudflare/latest/docs/resources/record
 resource "cloudflare_record" "plex" {
   zone_id = var.cloudflare_zone_id
   name    = "plex"
-  value   = "traefik.${local.domain_name}"
+  value   = "${cloudflare_record.traefik.name}.${local.domain_name}"
   type    = "CNAME"
   ttl     = 1
   proxied = true
+
+  allow_overwrite = true
 }
+
+# https://registry.terraform.io/providers/cloudflare/cloudflare/latest/docs/resources/record
+resource "cloudflare_record" "auth" {
+  zone_id = var.cloudflare_zone_id
+  name    = "auth"
+  value   = "${cloudflare_record.traefik.name}.${local.domain_name}"
+  type    = "CNAME"
+  ttl     = 1
+  proxied = true
+
+  allow_overwrite = true
+}
+
+# https://registry.terraform.io/providers/cloudflare/cloudflare/latest/docs/resources/record
+resource "cloudflare_record" "code_server" {
+  zone_id = var.cloudflare_zone_id
+  name    = "code-server"
+  value   = "${cloudflare_record.traefik.name}.${local.domain_name}"
+  type    = "CNAME"
+  ttl     = 1
+  proxied = true
+
+  allow_overwrite = true
+}
+
+# https://registry.terraform.io/providers/cloudflare/cloudflare/latest/docs/resources/record
+# resource "cloudflare_record" "plex" {
+#   zone_id = var.cloudflare_zone_id
+#   name    = "homeassistant"
+#   value   = "${cloudflare_record.traefik.name}.${local.domain_name}"
+#   type    = "CNAME"
+#   ttl     = 1
+#   proxied = true
+
+#   allow_overwrite = true
+# }
